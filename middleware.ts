278f41d8f51d1ae0ada35343bc7a99591527b66b
AryanMankame <aryan672002@gmail.com>
2024-01-27 01:39:06 +0530

    Cron Jobs

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   debug: true,
-  publicRoutes: ["/"]
 });
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
