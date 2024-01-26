8e454c3cda0ac651d841c4bb78f00cc86bdb6c0a
AryanMankame <aryan672002@gmail.com>
2024-01-27 01:26:49 +0530

    nextjs14tut

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
+  debug: true,
+  publicRoutes: ["/"]
 });
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
