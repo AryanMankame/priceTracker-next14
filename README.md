aaf00bf521ce8101b611ee37e3573564451140f1
AryanMankame <aryan672002@gmail.com>
2024-01-28 19:35:45 +0530

    Cron Jobs

--- README.md
+++ README.md
@@ -34,3 +34,62 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+import { fetchAllProducts, fetchHighestLowestAveragePrice, fetchUsersUsingProductId, updateProduct } from "@/lib/db"
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/scrapper";
+import { emailNotificationType } from "@/lib/scrapper/utils";
+import { NextResponse } from "next/server";
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+const executeOnYourOwn = async () => {
+    const THRESHOLD_RATE : Number = 40;
+    console.log('execute on your own function has started running...');
+    try{
+        const allProducts = await fetchAllProducts();
+        console.log('all the products have been fetched ', allProducts);
+        if(allProducts){
+            const updatedProducts = await Promise.all(
+                allProducts.map(async (product) => {
+                    // console.log(product.id)
+                    const scrapedData = await scrapeAmazonProduct(product.url);
+                    var priceHistory = await fetchHighestLowestAveragePrice(product.id,Number(scrapedData.currentPrice));
+                    const prodData = {
+                        id : product.id,
+                        ...scrapedData,
+                        ...priceHistory,
+                    }
+                    // console.log(prodData);
+                    const updateStatus = await updateProduct(prodData);
+                    const emailNotifType = await emailNotificationType(product,prodData);
+                    if(emailNotifType && updateStatus?.rowCount > 0){
+                        const prodInfo = {
+                            title : scrapedData.title,
+                            url : scrapedData.url
+                        }
+                        const emailBody = await generateEmailBody(prodInfo, emailNotifType);
+                        const usersTrackingProduct = await fetchUsersUsingProductId(product.id);
+                        console.log("users => ",product.id," => ",usersTrackingProduct," => ",emailBody);
+                        if(usersTrackingProduct && usersTrackingProduct.length > 0)
+                        await sendEmail(emailBody, usersTrackingProduct)
+                    }
+                    // console.log(updateStatus,emailNotifType)
+                    return prodData
+                })
+            ) 
+            console.log("allproductsafter update => ",updatedProducts)
+        }
+    }
+    catch(err){
+        throw Error(`error occurred ${err}`)
+    }
+}
+export async function GET(){
+    executeOnYourOwn();
+    console.log('started running....')
+    return NextResponse.json({
+        message: "Ok"
+    });
+}
--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,57 +1,61 @@
-import { fetchAllProducts, fetchHighestLowestAveragePrice, fetchUsersUsingProductId, updateProduct } from "@/lib/db"
+import { fetchAllProducts, fetchHighestLowestAveragePrice, fetchUsersUsingProductId, updateProduct } from "@/lib/db";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { emailNotificationType } from "@/lib/scrapper/utils";
 import { NextResponse } from "next/server";
-export const maxDuration = 10; // This function can run for a maximum of 300 seconds
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-const executeOnYourOwn = async () => {
-    const THRESHOLD_RATE : Number = 40;
-    console.log('execute on your own function has started running...');
-    try{
-        const allProducts = await fetchAllProducts();
-        console.log('all the products have been fetched ', allProducts);
-        if(allProducts){
-            const updatedProducts = await Promise.all(
-                allProducts.map(async (product) => {
-                    // console.log(product.id)
-                    const scrapedData = await scrapeAmazonProduct(product.url);
-                    var priceHistory = await fetchHighestLowestAveragePrice(product.id,Number(scrapedData.currentPrice));
-                    const prodData = {
-                        id : product.id,
-                        ...scrapedData,
-                        ...priceHistory,
-                    }
-                    // console.log(prodData);
-                    const updateStatus = await updateProduct(prodData);
-                    const emailNotifType = await emailNotificationType(product,prodData);
-                    if(emailNotifType && updateStatus?.rowCount > 0){
-                        const prodInfo = {
-                            title : scrapedData.title,
-                            url : scrapedData.url
-                        }
-                        const emailBody = await generateEmailBody(prodInfo, emailNotifType);
-                        const usersTrackingProduct = await fetchUsersUsingProductId(product.id);
-                        console.log("users => ",product.id," => ",usersTrackingProduct," => ",emailBody);
-                        if(usersTrackingProduct && usersTrackingProduct.length > 0)
-                        await sendEmail(emailBody, usersTrackingProduct)
-                    }
-                    // console.log(updateStatus,emailNotifType)
-                    return prodData
-                })
-            ) 
-            console.log("allproductsafter update => ",updatedProducts)
-        }
+
+export const runtime = 'edge';
+
+export async function GET() {
+  const encoder = new TextEncoder();
+
+  async function executeCronJob() {
+    try {
+      const allProducts = await fetchAllProducts();
+      if (allProducts) {
+        await Promise.all(
+          allProducts.map(async (product) => {
+            const scrapedData = await scrapeAmazonProduct(product.url);
+            const priceHistory = await fetchHighestLowestAveragePrice(product.id, Number(scrapedData.currentPrice));
+            const prodData = {
+              id: product.id,
+              ...scrapedData,
+              ...priceHistory,
+            };
+
+            const updateStatus = await updateProduct(prodData);
+            const emailNotifType = await emailNotificationType(product, prodData);
+
+            if (emailNotifType && updateStatus?.rowCount > 0) {
+              const prodInfo = {
+                title: scrapedData.title,
+                url: scrapedData.url,
+              };
+              const emailBody = await generateEmailBody(prodInfo, emailNotifType);
+              const usersTrackingProduct = await fetchUsersUsingProductId(product.id);
+
+              if (usersTrackingProduct && usersTrackingProduct.length > 0) {
+                await sendEmail(emailBody, usersTrackingProduct);
+              }
+            }
+
+            return prodData;
+          })
+        );
+      }
+    } catch (error) {
+      throw new Error(`Error occurred: ${error}`);
     }
-    catch(err){
-        throw Error(`error occurred ${err}`)
-    }
-}
-export async function GET(){
-    executeOnYourOwn();
-    console.log('started running....')
-    return NextResponse.json({
-        message: "Ok"
-    });
+  }
+
+  const customReadable = new ReadableStream({
+    async start(controller) {
+      await executeCronJob();
+      controller.close();
+    },
+  });
+
+  return new Response(customReadable, {
+    headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+  });
 }
