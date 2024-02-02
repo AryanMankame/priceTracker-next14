61e058f497a7f497f62fbd700c3525b86475db0f
AryanMankame <aryan672002@gmail.com>
2024-02-03 00:01:10 +0530

    Cron Jobs

---
+++ app/api/cron.js
@@ -0,0 +1,55 @@
+import { fetchAllProducts, fetchHighestLowestAveragePrice, fetchUsersUsingProductId, updateProduct } from "@/lib/db"
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/scrapper";
+import { emailNotificationType } from "@/lib/scrapper/utils";
+import { NextResponse } from "next/server";
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default async function handler(req, res) {
+    const THRESHOLD_RATE = 40;
+    try{
+        const allProducts = await fetchAllProducts();
+        if(allProducts){
+            const updatedProducts = await Promise.all(
+                allProducts.map(async (product) => {
+                    // console.log(product.id)
+                    const scrapedData = await scrapeAmazonProduct(product.url);
+                    var priceHistory = await fetchHighestLowestAveragePrice(product.id,Number(scrapedData.currentPrice));
+                    const prodData = {
+                        id : product.id,
+                        ...scrapedData,
+                        ...priceHistory,
+                    }
+                    // console.log(prodData);
+                    const updateStatus = await updateProduct(prodData);
+                    const emailNotifType = await emailNotificationType(product,prodData);
+                    if(emailNotifType && updateStatus?.rowCount > 0){
+                        const prodInfo = {
+                            title : scrapedData.title,
+                            url : scrapedData.url
+                        }
+                        const emailBody = await generateEmailBody(prodInfo, emailNotifType);
+                        const usersTrackingProduct = await fetchUsersUsingProductId(product.id);
+                        // console.log("users => ",product.id," => ",usersTrackingProduct," => ",emailBody);
+                        if(usersTrackingProduct && usersTrackingProduct.length > 0)
+                        await sendEmail(emailBody, usersTrackingProduct)
+                    }
+                    // console.log(updateStatus,emailNotifType)
+                    return prodData
+                })
+            ) 
+            // console.log("allproductsafter update => ",updatedProducts)
+            return NextResponse.json({
+                message: "Ok",
+                data: updatedProducts,
+            });
+        }
+    }
+    catch(err){
+        throw Error(`error occurred ${err}`)
+    }
+    res.status(200).end('Hello Cron!');
+}
+  
